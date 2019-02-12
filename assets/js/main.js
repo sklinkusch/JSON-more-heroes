e73756f87fba2404147efa5685530e32ed294110
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-02-12 14:16:44 +0100

    refactored it using parse

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,10 @@ let requestUrl =
   "https://gist.githubusercontent.com/LeandroDCI/697427913933937f714dc9c4e728d1c7/raw/ce065f1a72d937eade74e5523d68d834ec2b5304/Heroes.json";
 let request = new XMLHttpRequest();
 request.open("GET", requestUrl);
-request.responseType = "json";
+request.responseType = "text";
 request.send();
 request.onload = function() {
-  let superHeroes = request.response;
+  let HeroesText = request.response;
+  let superHeroes = JSON.parse(HeroesText);
   console.log(superHeroes);
 };
