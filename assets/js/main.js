9c64d571227c0c5b03ff0dbb02f317a41087e508
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-02-12 14:15:00 +0100

    http request done

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// Defining container
+container = document.querySelector("main");
+let requestUrl =
+  "https://gist.githubusercontent.com/LeandroDCI/697427913933937f714dc9c4e728d1c7/raw/ce065f1a72d937eade74e5523d68d834ec2b5304/Heroes.json";
+let request = new XMLHttpRequest();
+request.open("GET", requestUrl);
+request.responseType = "json";
+request.send();
+request.onload = function() {
+  let superHeroes = request.response;
+  console.log(superHeroes);
+};
